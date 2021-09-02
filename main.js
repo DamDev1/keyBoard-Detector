d35af330b46a814709b2e3b246df0d5db2e309f5
SadCoder-01 <fridaydamilare22@gmail.com>
2021-09-02 22:44:39 +0100

    first commit

--- main.js
+++ main.js
@@ -1,3 +1,6 @@
+// creating a window event listener 
 window.addEventListener("keydown", function(e) {
-    document.querySelector("#demo").textContent = e.key
+    //here when i get the id of h1 using querySelector
+    //so when ever the user click on his/her keyboard...i want it clear what is inside the h1..and change it to the user click
+    document.querySelector("#demo").textContent = e.key;
 })
